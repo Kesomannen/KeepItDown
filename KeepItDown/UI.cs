018e6a3e5bb1639e9bcf83c2d14d3b19515d3639
Kesomannen <bobbo.nidor@gmail.com>
2024-02-24 16:46:17 +0100

    Changed search

--- KeepItDown/UI.cs
+++ KeepItDown/UI.cs
@@ -7,26 +7,44 @@ using UnityEngine;
 
 namespace KeepItDown; 
 
-[DefaultExecutionOrder(-10)]
 public class UI : MonoBehaviour {
     const string Name = "Keep It Down!";
     const string Guid = PluginInfo.PLUGIN_GUID;
     const string Version = PluginInfo.PLUGIN_VERSION;
     const string Description = "Volume control for various sounds in the game.";
     
-    readonly Dictionary<string, SliderComponent> _sliders = new();
+    SliderComponent[] _sliders;
+    
+    readonly Dictionary<SliderComponent, string> _sliderToConfigKey = new();
 
     public void Awake() {
         var config = KeepItDownPlugin.Instance.Config;
-        var sliders = config.Volumes.Values.Select(CreateSlider).OrderBy(slider => slider.Text);
+        _sliders = config.Volumes.Select(kvp => {
+            kvp.Value.OnChanged += OnConfigChangedHandler;
+            
+            return new SliderComponent {
+                MinValue = 0,
+                MaxValue = 100,
+                OnValueChanged = OnSliderValueChanged
+            };
+        }).ToArray();
+        RefreshOrder();
 
         var resetButton = new ButtonComponent {
             Text = "Reset",
             OnClick = ResetSliders
         };
 
-        var components = new List<MenuComponent> { resetButton, };
-        components.AddRange(sliders);
+        var searchBar = new InputComponent {
+            Placeholder = "Search...",
+            OnValueChanged = (_, text) => RefreshOrder(text)
+        };
+
+        var components = new List<MenuComponent> {
+            resetButton,
+            searchBar
+        };
+        components.AddRange(_sliders);
         
         ModMenu.RegisterMod(new ModMenu.ModSettingsConfig {
             Name = Name,
@@ -36,35 +54,47 @@ public class UI : MonoBehaviour {
             MenuComponents = components.ToArray()
         }, true, true);
     }
-
-    SliderComponent CreateSlider(VolumeConfig config) {
-        var text = $"{NicifyName(config.Key)} Volume";
-        if (config.Section != "Vanilla") {
-            text += $" ({config.Section})";
+    
+    void RefreshOrder(string searchTerm = null) {
+        var config = KeepItDownPlugin.Instance.Config;
+        
+        IEnumerable<string> orderedKeys;
+        if (searchTerm == null) {
+            orderedKeys = config.Volumes.Keys.OrderBy(k => k);
+        } else {
+            var lowerSearchTerm = searchTerm.ToLower();
+            orderedKeys = config.Volumes.Keys
+                .OrderBy(k => k.ToLower().Contains(lowerSearchTerm) ? 0 : 1);
         }
         
-        var component = new SliderComponent {
-            Text = text,
-            MinValue = 0,
-            MaxValue = 100,
-            Value = config.RawValue,
-            OnValueChanged = OnSliderValueChanged
-        };
-        
-        config.OnChanged += OnConfigChangedHandler;
-
-        _sliders[config.Key] = component;
-        return component;
+        _sliderToConfigKey.Clear();
+        var i = 0;
+        foreach (var key in orderedKeys) {
+            var slider = _sliders[i++];
+            var volumeConfig = config.Volumes[key];
+            
+            var text = $"{key} Volume";
+            if (volumeConfig.Section != "Vanilla") {
+                text += $" ({volumeConfig.Section})";
+            }
+            
+            slider.Text = text;
+            slider.Value = volumeConfig.RawValue;
+            
+            _sliderToConfigKey[slider] = key;
+        }
     }
     
     void OnSliderValueChanged(SliderComponent slider, float value) {
-        var key = _sliders.First(kvp => kvp.Value == slider).Key;
-        if (!KeepItDownPlugin.TryGetConfig(key, out var volumeConfig)) return;
-        volumeConfig.RawValue = value;
+        if (!_sliderToConfigKey.TryGetValue(slider, out var key)) return;
+        if (!KeepItDownPlugin.TryGetConfig(key, out var config)) return;
+        
+        config.RawValue = value;
     }
 
     void OnConfigChangedHandler(VolumeConfig config, float rawValue, float normalizedValue) {
-        var slider = _sliders[config.Key];
+        var slider = _sliders.FirstOrDefault(s => _sliderToConfigKey[s] == config.Key);
+        if (slider == null) return;
         if (Mathf.Approximately(slider.Value, rawValue)) return;
         slider.Value = rawValue;
     }
@@ -73,11 +103,5 @@ public class UI : MonoBehaviour {
         foreach (var volumeConfig in KeepItDownPlugin.Instance.Config.Volumes.Values) {
             volumeConfig.NormalizedValue = 1f;
         }
-    }
-    
-    readonly Regex _camelCaseRegex = new("([a-z])([A-Z])");
-
-    string NicifyName(string name) {
-        return _camelCaseRegex.Replace(name, "$1 $2");
     }
 }
